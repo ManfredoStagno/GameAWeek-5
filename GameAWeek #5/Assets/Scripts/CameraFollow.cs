db1e4462677770476964777c23ec6f10e5252b63
Manfredo Stagno d'Alcontres <manfredo@stagno.it>
2019-06-16 17:41:25 +0200

    Okay, we got something. Move and jump working perfectly.

---
+++ GameAWeek #5/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform player;
+    private Camera cam;
+
+    public Vector3 offset;
+    public float smoothSpeed;
+
+    public Vector3 lookAtOffset;
+
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = player.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+
+        transform.LookAt(player.position + lookAtOffset);
+
+    }
+}
---
+++ GameAWeek #5/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    CharacterController cc;    
+
+    Vector3 moveVector = Vector3.zero;
+    public float speed;
+    public float jumpSpeed;
+
+    public float gravity;
+    [Range(0,1)]
+    public float highJumpMultiplier;
+
+
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    void FixedUpdate()
+    {
+        MoveAndJump();  
+    }
+
+    void MoveAndJump()
+    {
+        float appliedGravity = gravity * Time.deltaTime; //delta time not needed, but useful for tuning
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (cc.isGrounded)
+            {
+                moveVector.y = jumpSpeed;
+            }
+
+            if (moveVector.y > 0)
+            {
+                appliedGravity *= highJumpMultiplier;
+            }
+        }
+
+        moveVector.x = speed;
+        moveVector.y -= appliedGravity;
+
+        cc.Move(moveVector * Time.deltaTime);
+    } 
+}
